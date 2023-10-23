112f85ac7a6c174837a23c235063050ba819b0c1
viditjain44 <146834341+viditjain44@users.noreply.github.com>
2023-10-23 16:28:46 +0530

    Add files via upload

---
+++ sorting.js
@@ -0,0 +1,11 @@
+const n=10;
+const array=[];
+ 
+for(let i=0; i<n; i++){
+    array[i]=Math.random();
+}
+for(let i=0; i<array.length; i++){
+    const bar=document.createElement("div");
+    bar.style.height=array[i]*100+"%";
+    bar.style.width="10px";
+}
